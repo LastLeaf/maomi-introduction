41d5179b006797614239f4ff84ff9dd21680e629
LastLeaf <bqfu@163.com>
2023-02-21 23:38:01 +0800

    refactor: initial coding

--- build.rs
+++ build.rs
@@ -1,12 +1,12 @@
 fn main() {
-    println!("cargo:rerun-if-env-changed=LANG");
-    if let Ok(lang) = std::env::var("LANG") {
-        let locale = lang.split('.').next().unwrap();
-        if locale != "C" {
-            println!(
-                "cargo:rustc-env=MAOMI_I18N_LOCALE={}",
-                locale,
-            );
-        }
-    }
+    // println!("cargo:rerun-if-env-changed=LANG");
+    // if let Ok(lang) = std::env::var("LANG") {
+    //     let locale = lang.split('.').next().unwrap();
+    //     if locale != "C" {
+    //         println!(
+    //             "cargo:rustc-env=MAOMI_I18N_LOCALE={}",
+    //             locale,
+    //         );
+    //     }
+    // }
 }
