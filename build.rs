1413f10e30989d8f827209d0e0904efaed528498
LastLeaf <bqfu@163.com>
2023-02-22 19:50:56 +0800

    refactor: initial coding

--- build.rs
+++ build.rs
@@ -1,8 +1,4 @@
 fn main() {
-    println!(
-        "cargo:rustc-env=MAOMI_I18N_LOCALE={}",
-        "zh_CN",
-    );
     println!("cargo:rerun-if-env-changed=LANG");
     if let Ok(lang) = std::env::var("LANG") {
         let locale = lang.split('.').next().unwrap();
